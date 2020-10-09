273693afc198ed993fb71eb8086431054a5349ef
RoyalTwo <kissellogan10@gmail.com>
2020-10-09 05:55:52 +0000

    stuff

--- index.ts
+++ index.ts
@@ -1,28 +1,53 @@
-var app = require('express')();
-var http = require('http').createServer(app);
-var io = require('socket.io')(http);
+const app = require('express')();
+const server = require('http').createServer(app);
+const io = require('socket.io')(server);
+var mysql = require('mysql');
+
+var con = mysql.createConnection({
+  host: "162.237.71.9",
+  user: "root",
+  password: "rootpass"
+});
+
+con.connect(function(err) {
+  if (err) throw err;
+  console.log("Connected!");
+});
 
 // html for page
 app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/index.html');
+    res.sendFile(__dirname + '/main/home-doc.html');
 });
 
 // user connection
 io.on('connection', (socket) => {
-  console.log('a user connected');
-  socket.on('disconnect', () => {
-    console.log('user disconnected');
-  });
+    console.log('a user connected');
+    socket.on("userinfo", (infObj) => {
+
+    })
 });
 
 // display message to all on server
-  io.on('connection', (socket) => {
+io.on('connection', (socket) => {
     socket.on('chat message', (msg) => {
-      io.emit('chat message', msg);
+        io.emit('chat message', msg);
     });
-  });
+});
 
 // initialize site
-http.listen(3000, () => {
-  console.log('listening on *:3000');
+server.listen(8080, () => {
+    console.log('listening on *:8080');
 });
+
+/*
+
+infObj = {
+
+    uname: Admin,
+    pass: P4$$wd432,
+    token: <16CharValue>,
+
+}
+
+
+*/
